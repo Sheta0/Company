446eeba987d44ae702ba569206aa5e8ad780d73f
Sheta0 <theimpossible000@gmail.com>
2025-03-27 13:01:52 +0200

    Forgot Password / Reset Password

---
+++ Company.PL/DTOs/ForgotPasswordDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.PL.DTOs
+{
+    public class ForgotPasswordDTO
+    {
+        [Required(ErrorMessage = "Email is Required!")]
+        [EmailAddress(ErrorMessage = "Email is not valid!")]
+        public string Email { get; set; }
+    }
+}
